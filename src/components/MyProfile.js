62a9e0fc448afd502b41118d26170be6e38cf56f
gcampbell391 <gcampbell391@gmail.com>
2020-06-14 12:57:23 -0400

    Updated Image

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -3,7 +3,7 @@ import React from "react"
 const MyProfile = () => {
     return (
         <div className="myProfile">
-            <img src={require("../images/mySelfie.png")} id="selfieImg" alt="Selfie" />
+            <img src={require("../images/mySelfie.JPG")} id="selfieImg" alt="Selfie" />
             <p id="nameProfileHeader">Gene Campbell III</p>
             <p id="bioProfileHeader">Full Stack Software Engineer</p>
         </div>
