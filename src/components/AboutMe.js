60334b62142e08af7c977e12eb26b18a87ae3a61
gcampbell391 <gcampbell391@gmail.com>
2020-07-08 17:39:23 -0400

    updated deployed app and weekly section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,7 @@ const AboutMe = () => {
             <p id="aboutMeTitle">About Me</p>
             <p className="aboutMeParagraph">I have 3 years experience working with programming. I don't have any professional experience
             yet as a software engineer, but I'm currently seeking employment. I have experience working
-            with Java, C#, Ruby, Rails, Javascript, React, Redux, SQL, HTML, and CSS. I aspire to be a
+            with Java, C#, Ruby, Rails, Javascript, React, Redux, Node js, Express js, MongoDB, SQL, HTML, and CSS. I aspire to be a
             full stack engineer as I enjoy working with the front end and back end. I have always been
             fascinated with technology. I love the logic, algorithms, and puzzles that come along with
             programming.  I'm currently looking for a jr developer or entry level position as a software
--- src/components/CurrentProject.js
+++ src/components/CurrentProject.js
@@ -5,10 +5,14 @@ const CurrentProject = () => {
     return (
         <div className="currentProject">
             <h1 className="currentProjectTitle">What Did I Do This Week?</h1>
-            <p className="currentProjectDesc">The current week is 6/28/20 - 7/4/20. I'm about to finish up my Node js course, and next week I plan
-            on completing an AWS course. I'm currently working on a chat application built with Node js and Socket.io. I added a Deployment section to my
-            website now that I've deployed a couple applications. I plan on adding more applications to that section as I continue to grow and learn as a
-            software engineer. I start my offical job search with Flatiron next week after completing Mod 6!
+            <p className="currentProjectDesc">The current week is 7/5/20 - 7/11/20. I finally completed my Node js course on Udemy!
+            I have another deployed application to show off my new Node js skills. You can check it out below in the deployed section or by
+            clicking here: <a href="https://chit-chatter-app.herokuapp.com/" target="_blank" rel="noopener noreferrer">Chit Chatter</a>.
+            It's a chat application created with Node js, express, and socket.io. I started my official job search with Flatiron on Monday
+            July the 6th! I'm very excited to land my first job as a software engineer. I also posted a new <a href="https://medium.com/@gcampbell391/the-real-og-open-graph-tags-8c681361b7f4" target="_blank" rel="noopener noreferrer">blog</a> .
+            It's about Open Graph tags, and how they can help your applications stand out on social media platforms! I updated my deployed app section
+            to utilize react-multi-carousel instead of a flex-box. I think it gives off a cleaner look, and I have always been a fan of the image carousels
+            on other websites. I plan on learning React Native next to further strengthen my Javascript skills. Plus I already love React js!
             Check back next week to see what I'm working on next!</p>
         </div>
     )
