c78058a952608964b0b61cea282f990ac5384459
gcampbell391 <gcampbell391@gmail.com>
2020-07-24 16:40:27 -0400

    weekly section  updated

--- src/components/CurrentProject.js
+++ src/components/CurrentProject.js
@@ -10,14 +10,13 @@ const CurrentProject = () => {
         <div className="currentProject">
             <Bounce left cascade>
                 <h1 className="currentProjectTitle">What Did I Do This Week?</h1>
-                <p className="currentProjectDesc">The current week is 7/12/20 - 7/18/20. As you can tell I revamped my portfolio site! I was
-                inspired by seeing other software developers post their portfolio sites on LinkedIn. There is still a little bit more I would like
-                to change, but overall I think the new look is much better. My favorite new addition is the moving trees which was created using only css, and
-                I used React Typist to display all my technologies above. I wrote a new blow about React Carousels and how to easily implement them.
-            You can check out the blog <a href="https://medium.com/@gcampbell391/react-carousels-around-we-go-8f615fc7730b" target="_blank" rel="noopener noreferrer">here</a>.
-            I also spent time networking and applying to jobs for a good portion of this past week. I started a React Native course, but I haven't made it that deep into
-            the course yet. I plan on spending more time on the course next week.
-            Check back next week to see what I'm working on next!</p>
+                <p className="currentProjectDesc">The current week is 7/19/20 - 7/25/20. I spent this week working on my React Native course. I
+                completed my first React Native application! It is a simple goal app that allows a user to click a button to display a goal form.
+                The user can type a goal in the input and add it to the current goal list. All the goals are listed on the home screen. A user can
+                check off or delete a goal by clicking it. It's pretty simple, but it is my first React Native app! I can't wait to build something more sophisticated next!
+                I wrote a blog about how to easily implement a video onto a website. You can check out the blog
+                    <a href="https://medium.com/@gcampbell391/get-ready-to-react-to-videos-for-react-768e1425d5d1" target="_blank" rel="noopener noreferrer">here</a>.
+                    Check back next week to see what I'm working on next!</p>
             </Bounce>
         </div>
     )
