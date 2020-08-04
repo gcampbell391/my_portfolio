b445c1cde5824d941502230269bc50e627477aad
gcampbell391 <gcampbell391@gmail.com>
2020-08-04 14:39:09 -0400

    fixed unclickable icons

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -10,20 +10,20 @@ const Interests = () => {
             <Bounce left cascade>
                 <h1 id="interestTitle">Hobbies & Interests</h1>
                 <ul className="interestList">
-                    <li><Icon name="keyboard" link size="large" color='orange' /> Programming</li>
-                    <li><Icon name="bicycle" link size="large" color='red' /> Exercising</li>
-                    <li><Icon name="game" link size="large" color='teal' /> Video Gaming</li>
-                    <li><Icon name="plane" link size="large" color='olive' /> Traveling</li>
+                    <li className='disabledIcon'><Icon name="keyboard" link size="large" color='orange' /> Programming</li>
+                    <li className='disabledIcon'><Icon name="bicycle" link size="large" color='red' /> Exercising</li>
+                    <li className='disabledIcon'><Icon name="game" link size="large" color='teal' /> Video Gaming</li>
+                    <li className='disabledIcon'><Icon name="plane" link size="large" color='olive' /> Traveling</li>
                 </ul>
             </Bounce>
             <Bounce right cascade>
                 <h1 id="educationTitle">Accomplishments</h1>
                 <ul className="interestList">
                     <li><a href="https://www.chattahoocheetech.edu/computerprogramming/" target="_blank" rel="noopener noreferrer"> <Icon name="certificate" color='yellow' link size="large" /> Associate's Degree of Applied Science</a> in Computer Programming</li>
-                    <li><Icon name="code branch" color='red' link size="purple" /> Flatiron Full-Time Software Engineering Grad</li>
-                    <li><Icon name="circle check outline" color='green' link size="large" /> Certified in Java </li>
-                    <li><Icon name="circle check outline" color='green' link size="large" /> Certified in C# </li>
-                    <li><Icon name="address card outline" color='red' link size="large" /> Inducted Member of <a href="https://www.nsls.org/" target="_blank" rel="noopener noreferrer">NSLS</a></li>
+                    <li className='disabledIcon'><Icon name="code branch" color='red' link size="purple" /> Flatiron Full-Time Software Engineering Grad</li>
+                    <li className='disabledIcon'><Icon name="circle check outline" color='green' link size="large" /> Certified in Java </li>
+                    <li className='disabledIcon'><Icon name="circle check outline" color='green' link size="large" /> Certified in C# </li>
+                    <li><Icon name="address card outline" color='red' link size="large" className='disabledIcon' /> Inducted Member of <a href="https://www.nsls.org/" target="_blank" rel="noopener noreferrer">NSLS</a></li>
                 </ul>
             </Bounce>
         </div>
