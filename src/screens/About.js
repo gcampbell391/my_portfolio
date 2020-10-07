3077323a14a23971350eb20dd2bf664815870a42
gcampbell391 <gcampbell391@gmail.com>
2020-10-07 15:05:37 -0400

    working on smoke

--- src/screens/About.js
+++ src/screens/About.js
@@ -13,8 +13,23 @@ const About = () => {
         <div className='aboutScreen'>
             <Header />
             <div className="carouselContainer">
-                <ImageCarousel />
-                <InterestsCarousel />
+                {/* <ImageCarousel />
+                <InterestsCarousel /> */}
+                <section id='my-profile-section'>
+                    <h1 id='my-profile-section-header'>
+                        <span>A</span>
+                        <span>B</span>
+                        <span>O</span>
+                        <span>U</span>
+                        <span>T</span>
+                        <span>-</span>
+                        <span>M</span>
+                        <span>E</span>
+                    </h1>
+                    <div id='video-container'>
+                        <video src={require('../videos/smoke.mp4')} autoPlay muted></video>
+                    </div>
+                </section>
             </div>
             <AboutMe />
             <Interests />
