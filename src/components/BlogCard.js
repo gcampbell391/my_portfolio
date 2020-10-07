b92763030764ac2aa1cfcdc33fa361dc610fe1c2
gcampbell391 <gcampbell391@gmail.com>
2020-10-07 12:43:56 -0400

    updated blogs

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -26,6 +26,9 @@ const BlogCard = (props) => {
     if (props.blog.title === "7 Tips for Solving Algorithms and Coding Challenges") {
         finalBlogDescription = "Hello there, lately I’ve been working with a great deal of algorithms and coding challenges as I continue my journey for my first full time position as a software engineer! More than likely, you will have to complete a coding challenge or solve an algorithm to acquire a job in software developmen..."
     }
+    if (props.blog.title === "How To Dynamically Render Your Medium Blog Posts On Your Portfolio Site") {
+        finalBlogDescription = "If you haven’t read any of my previous articles or posts, I’m a software developer who is currently searching for his first full time position. In the meantime, I write weekly blogs and work on personal coding projects. The first project I created after I graduated from Flatiron was my Portfol..."
+    }
     return (
         <div className='blog-card'>
             <div className='blog-card-left'>
@@ -36,6 +39,7 @@ const BlogCard = (props) => {
                 <p>
                     <span className='blog-card-bold-text'>Published Date:</span> <span>{props.blog.pubDate.split(" ")[0]}</span>
                 </p>
+                <span className='blog-card-bold-text'>Description:</span>
                 <p className='blog-card-description'>{finalBlogDescription}</p>
                 <p className='blog-card-bold-text'>Want to read more?</p>
                 <a href={props.blog.link} target="_blank" rel="noopener noreferrer">
