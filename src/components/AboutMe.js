a18e33c8563f8279e58dce909e02447a738d286b
gcampbell391 <gcampbell391@gmail.com>
2020-11-11 14:42:53 -0500

    updated about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,12 +7,11 @@ const AboutMe = () => {
     return (
         <RubberBand left cascade>
             <div className="aboutMe">
-                <p className="aboutMeParagraph">I have 3 years experience working with programming, and I'm ready for my first professional
-                role as a software engineer. I have experience working
-                with Java, C#, Ruby, Rails, Javascript, React, Redux, Node js, Express js, MongoDB, SQL, HTML, and CSS. I aspire to be a
+                <p className="aboutMeParagraph">I have been designing and building full stack applications for over 3 years now. I have experience working
+                with technologies such as Java, C#, Ruby, Rails, Javascript, React, Redux, Node js, Express js, MongoDB, SQL, HTML, and CSS. I aspire to be a
                 full stack engineer as I enjoy working with the front end and back end. I have always been
                 fascinated with technology. I love the logic, algorithms, and puzzles that come along with
-                programming. I'm currently looking for employment, so please feel free to contact me today! </p>
+                programming.</p>
             </div>
         </RubberBand>
     )
